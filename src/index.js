c1967ebda1c67cd130f05204046c7c5083709556
taehoedu <taehoedu@gmail.com>
2024-07-03 15:21:03 +0900

    complete task cra_cls_jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
-import './01/cra_cls_js/style.css'
+// import './01/cra_cls_js/style.css';
+import './01/cra_cls_jsx/style.css';
 import reportWebVitals from './reportWebVitals';
-import Wrap from './01/cra_cls_js/Wrap';
+// import Wrap from './01/cra_cls_js/Wrap';
+import Wrap from './01/cra_cls_jsx/Wrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
