33eeee5e8f81382fc168019a1d1b80f61cb1ab28
taehoedu <taehoedu@gmail.com>
2024-07-03 14:19:16 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import './01/cra_cls_js/style.css'
 import reportWebVitals from './reportWebVitals';
+import Wrap from './01/cra_cls_js/Wrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    // <App />
+    <Wrap />
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
