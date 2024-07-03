8bbd2b42e3139ffcbe66e22edaedf82df8cdc757
taehoedu <taehoedu@gmail.com>
2024-07-03 16:11:22 +0900

    complete task cra_fun_jsx

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import './01/cra_cls_js/style.css';
 // import './01/cra_cls_jsx/style.css';
-import './01/cra_fun_js/style.css';
+// import './01/cra_fun_js/style.css';
+import './01/cra_fun_jsx/style.css';
 import reportWebVitals from './reportWebVitals';
 // import Wrap from './01/cra_cls_js/Wrap';
 // import Wrap from './01/cra_cls_jsx/Wrap';
-import Wrap from './01/cra_fun_js/Wrap';
+// import Wrap from './01/cra_fun_js/Wrap';
+import Wrap from './01/cra_fun_jsx/Wrap';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
